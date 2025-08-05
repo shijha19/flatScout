196a4da5e08ab146d139bca5921cddcb4b1fd9cb
shivam <shivamjha1907@gmail.com>
2025-08-05 22:17:49 +0530

    chatfix1

--- flatScout/frontend/src/pages/ChatPage.jsx
+++ flatScout/frontend/src/pages/ChatPage.jsx
@@ -1,23 +1,15 @@
 import React, { useState } from 'react';
 import ChatComponent from '../components/Chat';
-import DebugConnections from '../components/DebugConnections';
+// import DebugConnections from '../components/DebugConnections';
 import Layout from '../components/Layout';
 
 const ChatPage = () => {
-  const [showDebug, setShowDebug] = useState(false);
+  // const [showDebug, setShowDebug] = useState(false);
 
   return (
     <Layout>
-      <div className="h-[calc(100vh-64px)]">
-        {/* Debug toggle button */}
-        <button
-          onClick={() => setShowDebug(!showDebug)}
-          className="fixed top-20 right-4 z-50 bg-yellow-500 text-white px-3 py-1 rounded text-sm hover:bg-yellow-600"
-        >
-          {showDebug ? 'Hide Debug' : 'Show Debug'}
-        </button>
-        
-        {showDebug && <DebugConnections />}
+      <div>
+        {/* Debug button and DebugConnections removed */}
         
         <ChatComponent />
       </div>
